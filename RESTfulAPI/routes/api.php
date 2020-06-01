ea62df18a4e82724c33fd7d04e363a089b894b30
RehamMobarak <reham.a.mobarak@gmail.com>
2020-06-01 16:02:39 +0200

    sellerCategory

--- RESTfulAPI/routes/api.php
+++ RESTfulAPI/routes/api.php
@@ -30,6 +30,7 @@ Route::resource('buyers.categories', 'Buyer\BuyerCategoryController', ['only' =>
 /* Sellers */
 Route::resource('sellers', "Seller\SellerController", ['only' => ['index', 'show']]);
 Route::resource('sellers.transactions','Seller\SellerTransactionController',['only' => ['index']]);
+Route::resource('sellers.categories','Seller\SellerCategoryController',['only' => ['index']]);
 
 /* Products */
 Route::resource('products', "Product\ProductController", ['only' => ['index', 'show']]);
