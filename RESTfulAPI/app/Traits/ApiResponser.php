ecf9d1192fcb41cd21b7d72dba3267f3aa6cd688
RehamMobarak <reham.a.mobarak@gmail.com>
2021-03-02 21:10:29 +0200

    #29 Done, Returning the new responses after transformation

--- RESTfulAPI/app/Traits/ApiResponser.php
+++ RESTfulAPI/app/Traits/ApiResponser.php
@@ -19,12 +19,20 @@ trait ApiResponser
 
     protected function showAll(Collection $collection, $code = 200)
     {
-        return $this->successResponse(['data' => $collection], $code);
+        if ($collection->isEmpty()) {
+            return $this->successResponse(['data' => $collection], $code);
+        }
+        $transformer = $collection->first()->transformer; //property in models
+        $collection = $this->transformData($collection, $transformer); //convert collection to transformed collection
+        return $this->successResponse( $collection, $code);
     }
 
-    protected function showOne(Model $model, $code = 200)
+    protected function showOne(Model $instance, $code = 200)
     {
-        return $this->successResponse(['data' => $model], $code);
+        //we deleted 'data=> ' because fractal returns it already
+        $transformer = $instance->transformer; //the property in model
+        $instance = $this->transformData($instance,$transformer);//transform it
+        return $this->successResponse($instance, $code);
     }
 
     protected function showMessage($message, $code = 200)
@@ -32,9 +40,9 @@ trait ApiResponser
         return $this->successResponse(['data' => $message], $code);
     }
 
-    // protected function transformeData($data,$transformer){
-    //     $transformation = fractal($data, new $transformer);
-
-    //     return $transformation->toArray();
-    // }
+    protected function transformData($data, $transformer)
+    {
+        $transformation = fractal($data, new $transformer);
+        return $transformation->toArray();
+    }
 }
