6b490d0e3b7ee2ec993765592a099ef42cbde576
RehamMobarak <reham.a.mobarak@gmail.com>
2021-03-23 04:33:22 +0200

    #33 HATEOAS category links

--- RESTfulAPI/app/Transformers/CategoryTransformer.php
+++ RESTfulAPI/app/Transformers/CategoryTransformer.php
@@ -39,6 +39,29 @@ class CategoryTransformer extends TransformerAbstract
             'creationDate' =>(string)$category->created_at,
             'lastChangedDate' =>(string)$category->updated_at,
             'deletionDate' =>isset($category->deleted_at) ? (String)$category->deleted_at : null,
+
+            'links' =>[
+                [
+                    'rel' => 'self',
+                    'href' => route('categories.show', $category->id)
+                ],
+                [
+                    'rel' => 'categories.buyers',
+                    'href' => route('categories.buyers.index', $category->id)
+                ],
+                [
+                    'rel' => 'categories.sellers',
+                    'href' => route('categories.sellers.index', $category->id)
+                ],
+                [
+                    'rel' => 'categories.products',
+                    'href' => route('categories.products.index', $category->id)
+                ],
+                [
+                    'rel' => 'categories.transactions',
+                    'href' => route('categories.transactions.index', $category->id)
+                ],
+            ]
         ];
     }
 
